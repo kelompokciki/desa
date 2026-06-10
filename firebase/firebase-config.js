5886a70d9bbd74de7f246bc2dee656f7f1be0d92
kelompokciki <rizzcry354@gmail.com>
2026-06-10 00:39:02 +0000

    ubah nama desa v3

--- firebase/firebase-config.js
+++ firebase/firebase-config.js
@@ -7,6 +7,14 @@ const firebaseConfig = {
   appId: "YOUR_APP_ID"
 };
 
+const isFirebaseConfigValid = () => {
+  return !firebaseConfig.apiKey.startsWith('YOUR_') && !firebaseConfig.authDomain.startsWith('YOUR_');
+};
+
+if (!isFirebaseConfigValid()) {
+  console.warn('Firebase config belum terpasang. Ganti placeholder di firebase/firebase-config.js dengan konfigurasi Firebase yang valid.');
+}
+
 const firebaseApp = firebase.initializeApp(firebaseConfig);
 const firebaseAuth = firebaseApp.auth();
 const firebaseDb = firebaseApp.firestore();
@@ -14,3 +22,4 @@ const firebaseDb = firebaseApp.firestore();
 window.firebaseApp = firebaseApp;
 window.firebaseAuth = firebaseAuth;
 window.firebaseDb = firebaseDb;
+window.isFirebaseConfigValid = isFirebaseConfigValid;
